11bb957822a68227dc61cc4dab67f0ea70598b23
Joao Henrique <jhrodriguesit@gmail.com>
2026-07-30 17:43:35 +1000

    task 1 fix

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,4 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
 
-    return {'Response': response}
+    return response.text
