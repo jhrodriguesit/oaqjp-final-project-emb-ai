bf7c6aea2d9d9bb76d741efdff82dec8461a46e1
Joao Henrique <jhrodriguesit@gmail.com>
2026-07-30 18:31:05 +1000

    task 5 fix

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,8 +1,8 @@
 from EmotionDetection.emotion_detection import emotion_detector
 import unittest
 
-class TestSentimentAnalyzer(unittest.TestCase):
-    def test_sentiment_analyzer(self):
+class TestEmotionDetector(unittest.TestCase):
+    def test_emotion_detector(self):
         result_1 = emotion_detector('I am glad this happened')
         self.assertEqual(result_1['dominant_emotion'], 'joy')
         result_2 = emotion_detector('I am really mad about this')
